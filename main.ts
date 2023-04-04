fe90c1db7e9c97c50dc98dfbf8dbe5ceb8021acd
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2023-04-04 17:49:31 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+let angulo = 90
+serial.redirectToUSB()
+basic.clearScreen()
+basic.forever(function () {
+    serial.writeValue("x", pins.analogReadPin(AnalogPin.P2))
+    serial.writeValue("angulo", angulo)
+    if (pins.analogReadPin(AnalogPin.P4) > 600) {
+        angulo = 90
+    }
+    if (pins.analogReadPin(AnalogPin.P2) > 800) {
+        angulo += 10
+    }
+    if (pins.analogReadPin(AnalogPin.P2) < 100) {
+        angulo += -10
+    }
+    if (angulo > 180) {
+        angulo = 180
+    }
+    if (angulo < 0) {
+        angulo = 0
+    }
+    servos.P1.setAngle(angulo)
+    basic.pause(100)
+})
